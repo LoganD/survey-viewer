fa1aaa7859c1eae736faa1e903fa12f3a0ef3494
Logan Donovan <commit@logandonovan.com>
2017-10-11 02:33:33 -0400

    add qualification and quota components

--- survey-viewer/src/components.js
+++ survey-viewer/src/components.js
@@ -36,12 +36,33 @@ class Survey extends React.Component {
 }
 
 class SurveyQualifications extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  buildQualifications() {
+    return this.props.qualifications.map(qualification => {
+      return (
+        <SurveyQualification
+          {...qualification}
+          key={`{qualification.name} {qualification.allowed_values.join()}`}
+        />
+      );
+    });
+  }
+
+  toggleOpen = () => {
+    const toggled = !this.state.isOpen;
+    this.setState({ isOpen: toggled });
+  };
+
   render() {
     return (
-      <ul className="list-group list-group-flush">
+      <ul className="list-group list-group-flush" onClick={this.toggleOpen}>
         <li className="SurveyInfoHeader list-group-item bg-secondary text-white">
           Qualifications ({this.props.qualifications.length})
         </li>
+        {this.state.isOpen ? this.buildQualifications() : null}
       </ul>
     );
   }
@@ -49,25 +70,104 @@ class SurveyQualifications extends React.Component {
 
 class SurveyQualification extends React.Component {
   render() {
-    return <div />;
+    return (
+      <li className="SurveyAttribute list-group-item">
+        <div className="SurveyAttribute-InfoBlock card-block">
+          <p className="card-text mb-0">
+            Name: <text className="text-muted">{this.props.name}</text>
+          </p>
+          <p className="card-text mb-0">
+            Question:
+            <text className="text-muted">{this.props.question_text}</text>
+          </p>
+          <p className="card-text mb-0">
+            Logical Operator:
+            <text className="text-muted">{this.props.logical_operator}</text>
+          </p>
+        </div>
+        <div className="card-block">
+          <p className="card-text mb-0">
+            Allowed Values:
+            <small className="text-muted">
+              {this.props.qualification_precodes.join(", ")}
+            </small>
+          </p>
+        </div>
+      </li>
+    );
   }
 }
 
 class SurveyQuotas extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  buildQuotas() {
+    return this.props.quotas.map((quota, index) => {
+      return <SurveyQuota {...quota} key={`{quota.name} {index}`} />;
+    });
+  }
+
+  toggleOpen = () => {
+    const toggled = !this.state.isOpen;
+    this.setState({ isOpen: toggled });
+  };
+
   render() {
     return (
       <ul className="list-group list-group-flush">
-        <li className="SurveyInfoHeader list-group-item bg-secondary text-white">
-          Quotas ({this.props.quotas.length})
+        <li
+          className="SurveyInfoHeader list-group-item bg-secondary text-white"
+          onClick={this.toggleOpen}
+        >
+          Quota ({this.props.quotas.length})
         </li>
+        {this.state.isOpen ? this.buildQuotas() : null}
       </ul>
     );
   }
 }
 
 class SurveyQuota extends React.Component {
+  getClassNameForQuota() {
+    return this.props.quota_questions.length > 0
+      ? "SurveyAttribute--expandable SurveyAttribute list-group-item"
+      : "SurveyAttribute list-group-item";
+  }
+  buildQuestions(questions) {
+    return questions.map(question => {
+      return (
+        <SurveyQualification
+          {...question}
+          qualification_precodes={question.quota_question_precodes}
+          key={`{question.name} {question.quota_question_precodes.join()}`}
+        />
+      );
+    });
+  }
   render() {
-    return <div />;
+    const questions = this.props.quota_questions;
+    return (
+      <li className={this.getClassNameForQuota()}>
+        <p className="card-text mb-0">
+          Type:
+          <text className="text-muted">{this.props.survey_quota_type}</text>
+        </p>
+        <p className="card-text mb-0">
+          CPI:
+          <text className="text-muted">{this.props.quota_cpi}</text>
+        </p>
+        <p className="card-text mb-0">
+          Number of Respondents:
+          <text className="text-muted">{this.props.number_of_respondents}</text>
+        </p>
+        <p className="card-text mb-0">Quota Questions: ({questions.length})</p>
+        <span>
+          {questions.length > 0 ? this.buildQuestions(questions) : null}
+        </span>
+      </li>
+    );
   }
 }
 
