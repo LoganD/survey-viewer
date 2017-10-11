dfe1569851af07519ba184b5a57a910e3de0ef48
Logan Donovan <commit@logandonovan.com>
2017-10-11 01:11:39 -0400

    add basic view and styling for surveys

--- survey-viewer/src/components.js
+++ survey-viewer/src/components.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class SurveyContainer extends React.Component {
   buildSurverys() {
     return this.props.surveys.map(survey => {
-      return <Survey {...survey} />;
+      return <Survey {...survey} key={survey.id} />;
     });
   }
   render() {
@@ -13,14 +13,62 @@ class SurveyContainer extends React.Component {
 
 class Survey extends React.Component {
   render() {
+    const { qualifications, quotas, end_date } = this.props;
+    const endDate = new Date(Date.parse(end_date));
     return (
-      <div className="Survey col-md-4">
-        <div className="header">
-          <h2>{this.props.survey_name}</h2>
+      <div className="card Survey">
+        <div className="card-header bg-info text-white">
+          {this.props.survey_name}
         </div>
+        <div className="card-block SurveyInfo">
+          <h6 className="card-subtitle mb-2">
+            ID: <text className="text-muted">{this.props.id}</text>
+          </h6>
+          <h6 className="card-subtitle mb-2">
+            Ends on: <text className="text-muted">{endDate.toString()}</text>
+          </h6>
+        </div>
+        <SurveyQualifications qualifications={qualifications} />
+        <SurveyQuotas quotas={quotas} />
       </div>
     );
   }
 }
 
-export { Survey, SurveyContainer };
+class SurveyQualifications extends React.Component {
+  render() {
+    return (
+      <ul className="list-group list-group-flush">
+        <li className="SurveyInfoHeader list-group-item bg-secondary text-white">
+          Qualifications ({this.props.qualifications.length})
+        </li>
+      </ul>
+    );
+  }
+}
+
+class SurveyQualification extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+class SurveyQuotas extends React.Component {
+  render() {
+    return (
+      <ul className="list-group list-group-flush">
+        <li className="SurveyInfoHeader list-group-item bg-secondary text-white">
+          Quotas ({this.props.quotas.length})
+        </li>
+      </ul>
+    );
+  }
+}
+
+class SurveyQuota extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export { SurveyContainer };
